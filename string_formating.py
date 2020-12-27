b88c1a3e3479b15f8f35c870d1a07917e015d4ec
Heisenberg1607 <atharva_ak12@yahoo.in>
2020-12-27 18:31:23 +0530

    insert strings taken from user

---
+++ string_formating.py
@@ -0,0 +1,5 @@
+name= input("Enter your name:")
+surname= input("Ennter your surname:")
+
+messege= f"Hello {name} {surname}!" #f here is used to insert the strings between sentences
+print(messege)
